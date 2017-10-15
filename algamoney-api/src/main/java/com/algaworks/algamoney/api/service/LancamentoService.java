7559e1a1f866f5ccd7155509d58b6f7d36017dd4
DESKTOP-TINC0KG\igor2 <cunha.981@gmail.com>
2017-10-15 19:56:10 -0200

    tratando com bean validation lancamentos e pessoa inativa ou inexistente

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -7,10 +7,16 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algamoney.api.model.Lancamento;
+import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
+import com.algaworks.algamoney.api.repository.PessoaRepository;
+import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @Service
 public class LancamentoService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
@@ -28,6 +34,10 @@ public class LancamentoService {
 	}
 
 	public Lancamento salvar(Lancamento lancamento) {
+		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		if(pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
 		return lancamentoRepository.save(lancamento);
 	}
 
